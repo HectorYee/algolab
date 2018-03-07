3fa7c4cce2071cdbf7728b93ef7130ac86386e1a
hectoryee <hectoryeeos@gmail.com>
2018-03-07 15:56:08 +0800

    start lab 1

---
+++ algolab1.py
@@ -0,0 +1,16 @@
+chocolate = 0.7
+biscuit = 3.2
+sweet = 1
+
+print (chocolate+biscuit+sweet)
+
+print (biscuit+sweet)
+
+print (chocolate*3+biscuit+sweet)
+
+#
+bit = 6.60
+
+list=[1,2]
+list.append(3)
+print(list)

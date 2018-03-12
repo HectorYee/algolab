d06280dabde27a70e7b69940e57070cc9b3951e5
Hector Yee <hectoryeeos@gmail.com>
2018-03-13 02:18:17 +0800

    bucket

--- algolab1.py
+++ algolab1.py
@@ -2,27 +2,27 @@ import random
 
 # Question 1
 def sum3():
-    input = raw_input("Enter three numbers here: ")
+    input = input("Enter three numbers here: ")
     arr = [int(x) for x in input.split()]
     sum3 = 0
     for i in range(0, 3):
         sum3 += arr[i]
-    print sum3
+    print (sum3)
 
 # Question 2
 def got6():
-    input = raw_input("Enter a series of number: ")
+    input = input("Enter a series of number: ")
     arr = [int(x) for x in input.split()]
     got6 = False
     if arr[0] == 6 or arr[len(arr)-1] == 6:
             got6 = True
-    print got6
+    print (got6)
 
 # Question 3
 def even():
     even = 0
     for i in range(0, 101, 2):
-        print even + i
+        print (even + i)
 
 # Question 4
 def linear_search():
@@ -30,11 +30,11 @@ def linear_search():
     arr = random.sample(range(1000), 100)
     # for x in range(100):
     #     arr.append(map(int, random.sample(range(1000),1)))
-    print arr
+    print (arr)
     input1 = input("Enter a number: ")
     for x in range(0, len(arr)):
         if input1 == arr[x]:
-            print(x+1)
+            print ((x+1))
     return "Number not in the array"
 
 # Question 5
@@ -56,18 +56,18 @@ def fibonacci_iterative(n):
 
 # Question 6
 def rotate_left_3():
-    input = raw_input("Enter numbers: ")
+    input = input("Enter numbers: ")
     arr = [int(x) for x in input.split()]
     temp = arr[0]
     for x in range(0, len(arr)-1):
         arr[x] = arr[x+1]
     arr[len(arr)-1] = temp
-    print arr
+    print (arr)
 
 # Question 7
 def biggest_two():
-    input1 = raw_input("Enter first array of two numbers: ")
-    input2 = raw_input("Enter second array of two numbers: ")
+    input1 = input("Enter first array of two numbers: ")
+    input2 = input("Enter second array of two numbers: ")
     arr1 = [int(x) for x in input1.split()]
     arr2 = [int(x) for x in input2.split()]
     sum1 = 0
@@ -76,18 +76,18 @@ def biggest_two():
         sum1 += arr1[x]
         sum2 += arr2[x]
     if sum1 > sum2:
-        print arr1
+        print (arr1)
     elif sum2 > sum1:
-        print arr2
+        print (arr2)
     else:
-        print("a")
+        print (("a"))
 
 
 #=======================================================================================================================
 # Question 1
 def mapBully():
-    key_input  = raw_input("Enter key value: ").split()
-    value_input = raw_input("Enter value: ").split()
+    key_input  = input("Enter key value: ").split()
+    value_input = input("Enter value: ").split()
     map = {}
     for x in range(0, len(key_input)):
         if x == 0:
@@ -96,17 +96,17 @@ def mapBully():
             map[key_input[x]] = value_input[0]
         else:
             map[key_input[x]] = value_input[x]
-    print map
+    print (map)
 
 # Question 2
 def word0():
-    input = raw_input("Enter a string of keys: ").split()
+    input = input("Enter a string of keys: ").split()
     input = list(set(input))
-    print input
+    print (input)
     dict = {}
     for x in range(0, len(input)):
         dict[input[x]] = 0
-    print dict
+    print (dict)
 
 
 
@@ -145,8 +145,8 @@ def word0():
 # got6()
 # even()
 # linear_search()
-# print fibonacci_recursion(10)
-# print fibonacci_iterative(10)
+# print ( fibonacci_recursion(10)
+# print ( fibonacci_iterative(10)
 # rotate_left_3()
 # biggest_two()
 # mapBully()
---
+++ algolab2.py
@@ -0,0 +1,87 @@
+arr = [2,3,5,1,4,7,9]
+arr1 = [.3,.4,.1,.2,.6,.7,.9,.8,.7,.8]
+
+arrB = []
+for i in range(0, len(arr)):
+    arrB.append(0)
+print (arrB)
+
+print (arr)
+
+def bubble_sort(arr):
+    for i in range(0, len(arr)-2):
+        for j in range(len(arr)-1, i, -1):
+            if arr[j] < arr[j-1]:
+                temp = arr[j]
+                arr[j] = arr[j-1]
+                arr[j-1] = temp
+    print (arr)
+
+def counting_sort(arr, arrB, k):
+    c =[]
+    for i in range(0, k + 1):
+        c.append(0)
+    for j in range(0, len(arr)):
+        c[arr[j]] += 1
+    print (c)
+
+    for i in range(1, k+1):
+        c[i] = c[i] + c[i-1]
+    print (c)
+
+    print("B starts here")
+    for j in range(len(arr)-1, -1, -1):
+        arrB[c[arr[j]]-1] = arr[j]
+        print(arrB)
+        c[arr[j]] -= 1
+
+    print (arrB)
+
+def bucket_sort(arr):
+
+    n = len(arr)
+    bucket = [[]]*n
+    print(bucket)
+    for i in range(0, n):
+        bucket[i] = []
+    for i in range(0, n):
+        temp = int(n * arr[i])
+        bucket[temp].append(arr[i])
+    for i in range(0, n):
+        insertion_sort(bucket[i])
+
+    index = 0
+    for i in range(0, n):
+        for j in bucket[i]:
+            bucket[index] = j
+            index += 1
+    print (bucket)
+
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[i]
+            j = j - 1
+        arr[j + 1] = key
+    return arr
+
+
+
+
+
+
+
+
+bucket_sort(arr1)
+
+
+
+
+
+
+# bubble_sort(arr)
+# counting_sort(arr,arrB,9)
